02d1a1189ee87bc357559e56cf1b54bf05b4b317
larsswenson <94997829+larsswenson@users.noreply.github.com>
2022-04-30 16:41:57 -0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,22 +1,34 @@
-    export let guitars = [
-        { model : "les paul", make: "gibson", type: "electric solid archtop", year: 1952 },
-        { model : "telecaster", make: "fender", type: "electric solid body", year: 1954 },
-        { model : "330", make: "rickenbacker", type: "electric semi-hollow body", year: 1958 },
-        { model : "casino", make: "epiphone", type: "electric hollow body archtop", year: 1961 },
-        { model : "phantom", make: "vox", type: "electric hollow body", year: 1963 },
-    ];
+import { expect } from "chai";
+import * as guitar from "../data.js";
 
-    const getAll = () => {
-        return guitars;
-    }
-    
-    const getItem = (model) => {
-        return guitars.find((guitar) => {
-            return guitar.model === model;
-        });
-    }
-  
+describe("guitar", function() {
 
-    export { getAll, getItem };
-    
+    it("returns requested guitar", function() {
+        let result = guitar.getItem("telecaster");
+        expect(result).to.deep.equal({model: "telecaster", make: "fender", type: "electric solid body", year: 1954});
+    });
 
+    it("fails invalid guitar request", function() {
+        let result = guitar.getItem("invalid");
+        expect(result).to.be.undefined;
+    });
+
+    it("adds new guitar", function() {
+        let result = guitar.addItem({model: "starfire", make: "guild", type: "electric semi-hollow body", year: 1960});
+        expect(result.added).to.be.true;
+    });
+    it("fails adding listed guitar", function() {
+        let result = guitar.addItem({model: "les paul", make: "gibson", type: "electric solid archtop", year: 1952});
+        expect(result.added).to.be.false;
+    });
+
+    it("deletes listed guitar", function() {
+        let result = guitar.deleteItem("phantom");
+        expect(result.deleted).to.be.true;
+    });
+    it("fails deleting invalid guitar", function() {
+        let result = guitar.deleteItem("danelectro");
+        expect(result.deleted).to.be.false;
+    });
+
+});
--- index.js
+++ index.js
@@ -1,82 +1,56 @@
-/*import * as guitar from './data.js';
-import http from 'http';
-import { parse } from "querystring";
-http.createServer((req,res) => {
-    var path = req.url.toLowerCase();
-    let url_parts = req.url.split("?");  // separate route from query string
-    let query = parse(url_parts[1]); // convert query string to a JS object 
-    
-
-    switch(url_parts[0]) {
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(guitar.getAll());
-            break;
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end("Lars' About Page");
-            break;
-        case '/detail':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(guitar.getItem(query.model));
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Error, page not found');
-            break;
-    }
-}).listen(process.env.PORT || 3000);
-*/
-
-import * as guitar from './data.js';
-import express from 'express';
+import * as guitar from "./data.js";
+import express from "express";
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 app.use(express.static('./public')); // set location for static files
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // send static file as response
-app.get('/', (req,res) => {
-    res.type('text/html');
-    res.render('home', {guitars: guitar.getAll()});
+app.get("/", (req,res) => {
+    res.type("text/html");
+    console.log(req.query);
+    res.render("home", {guitars: guitar.getAll()});
    });
 
-app.get('/delete', (req,res) => {
-    let result = guitar.deleteItem(req.query.model);
-    res.render('delete', {model: req.query.model, result: result})
-   });
-
-app.get('/detail', (req,res) => {
-    res.type('text/html');
+app.get("/detail", (req,res) => {
+    res.type("text/html");
     console.log(req.query);
     let result = guitar.getItem(req.query.model);
-    res.render('detail', {model: req.query.model, result: result});  
+    res.render("detail", {model: req.query.model, result: result});  
+   });
+
+app.get("/add", (req,res) => {
+    res.type("text/html")
+    console.log(req.query)
+    let result = guitar.addItem(req.query.model);
+    res.render("add", {model: req.query.model, result: result})
+   });
+
+app.get("/delete", (req,res) => {
+    res.type("text/html")
+    console.log(req.query)
+    let result = guitar.deleteItem(req.query.model);
+    res.render("delete", {model: req.query.model, result: result})
    });
 
 // send plain text response
-app.get('/about', (req,res) => {
-    res.type('text/plain');
-    res.send('About page');
-   });
-
-app.post('/detail', (req,res) => {
-    console.log(req.body)
-    let found = guitar.getItem(req.body.model);
-    res.render("detail", {model: req.body.model, result: found, guitars: guitar.getAll()});
+app.get("/about", (req,res) => {
+    res.type("text/plain");
+    res.send("About page");
    });
 
 // define 404 handler
 app.use((req,res) => {
-    res.type('text/plain');
+    res.type("text/plain");
     res.status(404);
-    res.send('404 - Not found');
+    res.send("404 - Not found");
    });
 
-app.listen(app.get('port'), () => {
-    console.log('Express started');
+app.listen(app.get("port"), () => {
+    console.log("Express started");
    });
 
 
