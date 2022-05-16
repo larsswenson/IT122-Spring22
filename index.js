dcf34431432115a16af00ae496e430edafcf8f93
larsswenson <94997829+larsswenson@users.noreply.github.com>
2022-05-15 20:03:57 -0700

    rest api routes

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
 import * as guitar from "./data.js";
 import express from "express";
 import { Guitar } from "./Guitars.js";
+import cors from "cors";
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.use(express.static('./public')); // set location for static files
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.json());
+app.use('/api', cors());
 app.set("view engine", "ejs");
 
-app.get('/', (req, res, next) => {
+/*app.get("/", (req,res) => {
+    res.type("text/html");
+    console.log(req.query);
+    res.render("home", {guitars: guitar.getAll()});
+   });*/
+
+app.get('/api/guitars', (req,res) => {
     Guitar.find({}).lean()
         .then((guitars) => {
-            res.render('home', { guitars });
+            res.json(guitars)
         })
         .catch(err => next(err));
 });
 
-app.get('/detail', (req, res, next) => {
-    // db query can use request parameters
-    Guitar.findOne({ model:req.query.model }).lean()
+app.get('/api/guitars/:model', (req,res,next) => {
+    Guitar.findOne({ model:req.params.model }).lean()
         .then((guitar) => {
-            res.render('detail', {result: guitar} );
+            res.json(guitar)
         })
         .catch(err => next(err));
 });
 
-app.get("/add", (req, res) => {
-    res.type("text/html")
-    console.log(req.query)
-    let result = guitar.addItem(req.query.model);
-    res.render("add", {model: req.query.model, result: result})
-   });
-
-app.get('/delete', (req, res, next) => {
-    Guitar.deleteOne({ model:req.query.model }, (err, result) => {
+app.get('/api/guitars/delete/:model', (req,res) => {
+    Guitar.deleteOne({ model:req.params.model }, (err, result) => {
         if (err) return next(err);
         console.log(result)
-        let deleted = result.result.n !== 0;
-        Guitar.count((err, total) => {
-            res.type('text/html');
-            res.render('delete', {model: req.query.model, deleted: result.result.n !== 0, total: total } );    
-        });
+        res.json({"message": "guitar deleted"})    
+    });
+});
+
+app.post('/api/guitars/add', (req,res,next) => {
+    const newGuitar = {'model':'starfire', 'make':'guild', 'type': 'electric semi-hollow body', 'year': '1960'}
+    Guitar.updateOne({'model':'starfire'}, newGuitar, {upsert:true}, (err, result) => {
+        if (err) return next(err);
+        console.log(result);
+        res.json({"message": "guitar added"})
     });
 });
 
@@ -61,5 +66,6 @@ app.use((req, res) => {
 app.listen(app.get("port"), () => {
     console.log("Express started");
    });
+
 
 
